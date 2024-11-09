acfc08e6b58287d250c482df5673b3631fddc24c
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2024-11-09 17:26:34 +0300

    2.2

--- main.go
+++ main.go
@@ -14,32 +14,34 @@ func main() {
 	router.Static("/css", "./templates/css")
 	router.LoadHTMLGlob("templates/*.html")
 
-	router.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth())
+	router.Use(gin.Recovery(), middlewares.Logger())
 
 	router.GET("/", services.Main_Page)
 
-	router.GET("/donors", services.GetDonors)
-	router.GET("/donors/:id", services.DonorByID)
 	router.GET("/donor_form", services.ShowDonorForm)
-
-	router.GET("/patients", services.GetPatients)
-	router.GET("/patients/:id", services.PatientByID)
 	router.GET("/patient_form", services.ShowPatientForm)
 
-	router.POST("/donors", services.CreateDonor)
-	router.POST("/patients", services.CreatePatient)
-
-	router.POST("/donate-red", services.DonateRed)
-	router.POST("/donate-platelet", services.DonatePlatelet)
-
-	// Apply BasicAuth middleware only to match routes
 	matchRoutes := router.Group("/")
 	matchRoutes.Use(middlewares.BasicAuth())
 	{
+		router.GET("/donors", services.GetDonors)
+		router.GET("/donors/:id", services.DonorByID)
+
+		router.GET("/patients", services.GetPatients)
+		router.GET("/patients/:id", services.PatientByID)
+
+		router.POST("/donors", services.CreateDonor)
+		router.POST("/patients", services.CreatePatient)
+
+		router.POST("/donate-red", services.DonateRed)
+		router.POST("/donate-platelet", services.DonatePlatelet)
+
 		matchRoutes.GET("/match-red", services.MatchRedDonorPatient)
 		matchRoutes.GET("/match-platelet", services.MatchPlateletDonorPatient)
+
 		matchRoutes.GET("/match-red-ignore", services.MatchRedDonorPatientIgnoreBloodType)
 		matchRoutes.GET("/match-platelet-ignore", services.MatchPlateletDonorPatientIgnoreBloodType)
+
 		matchRoutes.GET("/match", services.ShowMatchForm)
 		matchRoutes.POST("/process-match", services.ProcessMatchForm)
 	}
