bd5cbb76f43c9f2025c25a611b9721cbd5bf6c8f
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2024-11-27 01:18:16 +0300

    Add API endpoints for scheduled requests: create, retrieve, and delete

--- handlers/request.go
+++ handlers/request.go
@@ -5,14 +5,40 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// CreateSchedualedRequest godoc
+// @Summary Create a schedualed request
+// @Description Create a schedualed request
+// @Tags request
+// @Accept  json
+// @Produce  json
+// @Param request body CreateSchedualedRequest true "Request"
+// @Success 200 {string} string "Schedualed request created"
+// @Router /scheduale-request [post]
 func CreateSchedualedRequest(c *gin.Context) {
 	services.CreateSchedualedRequest(c)
 }
 
+// GetAllSchedualedRequests godoc
+// @Summary Get all schedualed requests
+// @Description Get all schedualed requests
+// @Tags request
+// @Accept  json
+// @Produce  json
+// @Success 200 {array} models.SchedualedRequest "List of schedualed requests"
+// @Router /schedualed-requests [get]
 func GetAllSchedualedRequests(c *gin.Context) {
 	services.GetAllSchedualedRequests(c)
 }
 
+// DeleteScheduledRequest godoc
+// @Summary Delete a schedualed request
+// @Description Delete a schedualed request
+// @Tags request
+// @Accept  json
+// @Produce  json
+// @Param id path int true "ID of the request"
+// @Success 200 {string} string "Request deleted"
+// @Router /schedualed-request/{id} [delete]
 func DeleteScheduledRequest(c *gin.Context) {
 	services.DeleteScheduledRequest(c)
 }
