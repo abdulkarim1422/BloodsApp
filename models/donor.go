fe4159f21ae34175ac774d402d8d13751a7f9686
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2024-11-27 13:58:52 +0300

    Update Donor model and update_donor template: add transportation options and refine timer fields

--- models/donor.go
+++ models/donor.go
@@ -10,20 +10,20 @@ import (
 // @Description Donor represents a donor in the system
 type Donor struct {
 	gorm.Model
-	FirstName      string    `json:"first_name" form:"FirstName"`
-	LastName       string    `json:"last_name,omitempty" form:"LastName"`
-	LatinName      string    `json:"latinName,omitempty" form:"LatinName"` //form
-	PhoneNumber    string    `json:"phone_number" form:"PhoneNumber"`      //form
-	Email          string    `json:"email" form:"Email"`
-	BloodType      string    `json:"bloodType" form:"BloodType"` //form
-	MotherLanguage string    `json:"mother_language" form:"MotherLanguage"`
-	BirthDate      time.Time `json:"birth_date,omitempty" form:"BirthDate" time_format:"2006-01-02"`
-	Gender         string    `json:"gender" form:"Gender"`
-	Address        Address   `json:"address,omitempty" gorm:"embedded"`    //form*
-	Transportation string    `json:"transportation" form:"Transportation"` //form
-	Paid           bool      `json:"paid,omitempty"`
-	RedTimer       time.Time `json:"red_timer" time_format:"2006-01-02"`
-	PlateletTimer  time.Time `json:"platelet_timer" time_format:"2006-01-02"`
-	Score          int       `json:"score"`
-	Verify         string    `json:"verify,omitempty"`
+	FirstName      string    `json:"first_name" form:"FirstName"`                                    //none
+	LastName       string    `json:"last_name,omitempty" form:"LastName"`                            //none
+	LatinName      string    `json:"latinName,omitempty" form:"LatinName"`                           //FORM~
+	PhoneNumber    string    `json:"phone_number" form:"PhoneNumber"`                                //FORM~
+	Email          string    `json:"email" form:"Email"`                                             //none
+	BloodType      string    `json:"bloodType" form:"BloodType"`                                     //FORM~
+	MotherLanguage string    `json:"mother_language" form:"MotherLanguage"`                          //none
+	BirthDate      time.Time `json:"birth_date,omitempty" form:"BirthDate" time_format:"2006-01-02"` //none
+	Gender         string    `json:"gender" form:"Gender"`                                           //none
+	Address        Address   `json:"address,omitempty" gorm:"embedded"`                              //FORM~*
+	Transportation string    `json:"transportation" form:"Transportation"`                           //FORM~
+	Paid           bool      `json:"paid,omitempty"`                                                 //none
+	RedTimer       time.Time `json:"red_timer" time_format:"2006-01-02"`                             //auto
+	PlateletTimer  time.Time `json:"platelet_timer" time_format:"2006-01-02"`                        //auto
+	Score          int       `json:"score"`                                                          //auto
+	Verify         string    `json:"verify,omitempty"`                                               //auto
 }
