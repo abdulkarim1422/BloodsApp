e3a9e0d82c1f13911ff4bcc63ded8f54b7357e97
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2024-11-30 01:17:53 +0300

    Refactor JWT authentication middleware to use environment variable for secret and improve token validation logic

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -1,36 +1,63 @@
 package middlewares
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"time"
 
+	"github.com/abdulkarim1422/BloodsApp/repositories"
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v4"
 )
 
-var jwtKey = []byte("your_secret_key")
-
 func JWTAuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		tokenString, err := c.Cookie("token")
+		// Get the JWT token from the Authorization header in the Cookie
+		tokenString, err := c.Cookie("Autherization")
 		if err != nil {
 			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization cookie required"})
 			c.Abort()
 			return
 		}
 
-		claims := &jwt.RegisteredClaims{}
+		// Parse the JWT token
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+			}
 
-		token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
-			return jwtKey, nil
+			return []byte(os.Getenv("JWT_SECRET")), nil
 		})
-
-		if err != nil || !token.Valid {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
-			c.Abort()
-			return
+		if err != nil {
+			log.Fatal(err)
 		}
 
-		c.Set("username", claims.Subject)
-		c.Next()
+		// Check if the token is valid
+		if claims, ok := token.Claims.(jwt.MapClaims); ok {
+			// Chack the expiration time
+			if float64(time.Now().Unix()) > claims["ExpiresAt"].(float64) {
+				c.AbortWithStatus(http.StatusUnauthorized)
+			}
+
+			// Find the user in the database
+			user, err := repositories.GetUserByIdentifier(claims["username"].(string))
+			if err != nil {
+				c.AbortWithStatus(http.StatusUnauthorized)
+			}
+
+			if user.ID == 0 {
+				c.AbortWithStatus(http.StatusUnauthorized)
+			}
+
+			// Attach to req
+			c.Set("user", user)
+
+			// Continue
+			c.Next()
+		} else {
+			c.AbortWithStatus(http.StatusUnauthorized)
+		}
 	}
 }
